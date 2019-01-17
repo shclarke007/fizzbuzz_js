1c1300b949abfec35fae3e1e16fd7f452fd693e0
Shaneil Clarke <shaneilske@yahoo.com>
2019-01-17 17:09:10 +0000

    added interface.js to display front end

---
+++ interface.js
@@ -0,0 +1,8 @@
+let submit_button = document.getElementById('submit_button');
+
+submit_button.addEventListener('click', function() {
+  let game = new FizzBuzz;
+  let fizz_output = document.getElementById('fizz_output');
+  let num = document.getElementById('user_input').value;
+  fizz_output.textContent = game.play(num);
+});
